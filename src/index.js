73a3f2c1340a3dcaa149ffdc6b417707ed2b1457
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-02-12 00:10:33 +0900

    changed package.json to fix deploy need javascript error

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 // This stops scrolling
 document.body.style.overflowY = "hidden";
 
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 ReactDOM.render(
   <Router>
     <App />
